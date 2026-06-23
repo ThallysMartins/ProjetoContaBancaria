8721d2a009ee587c0b2a481344bf298bf1210859
Thallys Martins <devjoker95@gmail.com>
2026-06-23 16:00:03 -0300

    Fix: Ignorando pasta .vs e salvando metodos saque e depósito com loops e Try.Parse.

--- Sistema_Banco_Main.cs
+++ Sistema_Banco_Main.cs
@@ -24,27 +24,80 @@ namespace ProjetoFinança
 
         public void Depositar()
         {
-            Console.WriteLine("===  Deposite aqui seu dinheiro  ===");
-            float deposito = float.Parse(Console.ReadLine());
-            Saldo = Saldo + deposito;
-            Console.WriteLine($"Um deposito no valor de R${deposito} foi realizado. Seu novo saldo é de R${Saldo}.");
+            while (true)
+            {
+                Console.WriteLine("===  Deposite aqui seu dinheiro  ===");
+                Console.WriteLine("Digite o valor para deposito ou aperte \"V\" para voltar.");
+
+                string entrada = Console.ReadLine();
             
+                if (entrada.ToUpper() == "V")
+                {   
+                    break;
+                }
+
+                if (float.TryParse(entrada, out float deposito))
+                {
+                    if (deposito > 0)
+                    {
+                        Saldo = Saldo + deposito;
+                        Console.WriteLine($"Um deposito no valor de R${deposito} foi realizado. Seu novo saldo é de R${Saldo}.");
+                        break;
+                    }
+
+                    else
+                    {
+                        Console.WriteLine("Digite um valor maior que zero.");
+                    }
+                }
+                
+                else
+                {
+                    Console.WriteLine("Por favor, digite um número válido.");
+                }
+            }
         }
 
         public void Sacar()
         {
-            Console.WriteLine("=== Saque aqui seu dinheiro ===");
-            float saque = float.Parse(Console.ReadLine());
-            
-            if (Saldo >= saque)
+            while (true)
             {
-                Saldo = Saldo - saque;
-                Console.WriteLine($"Um saque no valor de R${saque} foi realizado. Seu novo saldo é de R${Saldo}.");
+                Console.WriteLine("=== Saque aqui seu dinheiro ===");
+                Console.WriteLine("Digite o valor do saque ou digite \"V\" para sair.");
+
+                string entrada = Console.ReadLine();
+
+                if (entrada.ToUpper() == "V")
+                {
+                    break;
+                }
+
+                if (float.TryParse(entrada, out float saque))
+                {
+                    if (saque > 0 && Saldo > saque)
+                    {
+                        Saldo = Saldo - saque;
+                        Console.WriteLine($"Um saque no valor de R${saque} foi realizado. Seu novo saldo é de R${Saldo}.");
+                        break;
+                    }
+
+                    else if (Saldo < saque)
+                    {
+                        Console.WriteLine("Você não tem saldo suficiente para sacar este valor.");
+                    }
+
+                    else if (saque <= 0)
+                    {
+                        Console.WriteLine("Valor de saque inválido! Digite uma valor maior que zero.");
+                    }
                 
-            }
-            else
-            {
-                Console.WriteLine("Seu saldo é insuficiente para realizar um saque.");
+                }
+                
+                else
+                {
+                    Console.WriteLine("Por favor, digite um número válido.");
+                }
+
             }
         }
 
